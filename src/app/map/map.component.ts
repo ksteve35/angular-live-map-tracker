82d45f5260c2a8ebdf5e9a765d7778f37674c69c
Kyle S <55467467+ksteve35@users.noreply.github.com>
2025-08-15 21:02:47 -0400

    Added JSON sim route files

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import { environment } from '@environments/environment'
+import route1Data from 'src/assets/routes/route1.json'
+import route2Data from 'src/assets/routes/route2.json'
+import route3Data from 'src/assets/routes/route3.json'
 import { Map as MapboxMap } from 'mapbox-gl'
 
 @Component({
@@ -10,6 +13,7 @@ import { Map as MapboxMap } from 'mapbox-gl'
 })
 export class MapComponent implements OnInit {
   private map!: MapboxMap
+  private routes: [number, number][][] = []
 
   ngOnInit(): void {
     // Initialize the Mapbox map
@@ -25,5 +29,16 @@ export class MapComponent implements OnInit {
       center: [-83.761868, 42.281902], // Initial map center as [longitude, latitude]
       zoom: 15 // Initial map zoom level
     })
+
+    // Initialize routes after the map has loaded and draw them on the map
+    this.map.on('load', () => {
+      this.initializeRoutes()
+    })
+  }
+
+  initializeRoutes(): void {
+    this.routes.push(route1Data.route[0] as [number, number][])
+    this.routes.push(route2Data.route[0] as [number, number][])
+    this.routes.push(route3Data.route[0] as [number, number][])
   }
 }
